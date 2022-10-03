efbe9ed6f58f8d8966e28b711aee71322060e904
Friya123 <32993909+Friya123@users.noreply.github.com>
2022-10-03 15:30:59 +0530

    Initial Coin offering Code

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xa4e79003DDfCAA95f9368561443A28b67C198aFd";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
